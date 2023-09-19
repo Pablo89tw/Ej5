06deeaa51609ed3e7fd66129e6eafa94dbae70dd
EgosEbrey <141878271+EgosEbrey@users.noreply.github.com>
2023-09-19 20:04:38 -0300

    probando2

--- src/ej5/Ej5.java
+++ src/ej5/Ej5.java
@@ -11,6 +11,7 @@ public class Ej5 {
     public static void main(String[] args) {
         System.out.println("GRANDE MOLLO CANTAUTOR");     
         System.out.println("probando");
+        System.out.println("probando2");
     }
     
 }
